5547fc38e46d402048a545115334d27e1978fda9
Roberth <htrebor20@gmail.com>
2023-11-24 01:25:48 -0500

    final project

---
+++ src/main/java/com/testmarket/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.testmarket.domain.repository;
+
+import com.testmarket.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
---
+++ src/main/java/com/testmarket/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,13 @@
+package com.testmarket.persistence.crud;
+
+import com.testmarket.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+}
